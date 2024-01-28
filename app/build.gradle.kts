4facec5e41988515bfdf94609d6721a6f073d0c3
Inaki Villar <inaki.seri@gmail.com>
2024-01-27 19:31:53 -0800

    a

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,10 +29,7 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+
     kotlin {
         jvmToolchain {
             languageVersion.set(JavaLanguageVersion.of(20))
